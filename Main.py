c1febf1599bb5ab8d406ecbb31b108fa5067e090
Carolina Martão <54716331+carolmartao@users.noreply.github.com>
2026-05-02 12:50:31 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="ElevateProfile IA", page_icon="🚀", layout="wid
 # Estilização para o Chat
 st.markdown("""
     <style>
-    .stChatMessage { border-radius: 15px; margin-bottom: 10px; }
+    .stChatMessage { border-radius: 15px; margin-bottom: 10px;  background-color: #000000;}
     [data-testid="stSidebar"] { background-color: #000000; }
     </style>
     """, unsafe_allow_html=True)
