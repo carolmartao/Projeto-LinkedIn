63f8feaac8e023e79db4626a5e6dda303281c962
Carolina Martão <54716331+carolmartao@users.noreply.github.com>
2026-05-02 12:45:20 -0300

    Mudança Layout

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="ElevateProfile IA", page_icon="🚀", layout="wid
 st.markdown("""
     <style>
     .stChatMessage { border-radius: 15px; margin-bottom: 10px; }
-    [data-testid="stSidebar"] { background-color: #D8D9DD; }
+    [data-testid="stSidebar"] { background-color: #000000; }
     </style>
     """, unsafe_allow_html=True)
 
